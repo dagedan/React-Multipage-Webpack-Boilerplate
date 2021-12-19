0bffb982629103bde7cf600c8dabbbcf572c31e5
dagedan <dagedan@gmail.com>
2021-12-19 18:18:47 +0800

    dev

--- src/Page1/index.jsx
+++ src/Page1/index.jsx
@@ -3,4 +3,4 @@ import "./css/index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1><a href="/IndexPage">这是一个冲突测试</a></h1>, document.getElementById("root"));
+ReactDOM.render(<h1><a href="/IndexPage">dev</a></h1>, document.getElementById("root"));
